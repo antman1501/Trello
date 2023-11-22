299fbb344ff4a4a19c0e089123d5f2e954a758dd
Antariksha Barua <antarikshabarua2000.ab@gmail.com>
2023-11-22 10:29:01 +0530

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
       <tokenContext.Provider value={Token}>
         <Stack sx={{width:'100%',height:'100vh',backgroundColor:'hsl(0, 0%, 98%)',borderRadius:'5px'}}>
           <Stack>
-            <Button onClick={(e)=>navigate('/')} ><img src="public/logo.png" height='50px'/></Button>
+            <Button onClick={(e)=>navigate('/')} variant='contained'>Trello</Button>
           </Stack>
           <Routes>
             <Route path='/' element={<Home />}>
